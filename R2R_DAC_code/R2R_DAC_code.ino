f6c19f9c3a6609ea4d7338e0bdb9dbe664f8dce0
Sam Baker <Sam.Baker101@gmail.com>
2020-11-24 00:02:22 -0500

    Timer Interrupts
    Setting up basic timer interrupts

--- R2R_DAC_code/R2R_DAC_code.ino
+++ R2R_DAC_code/R2R_DAC_code.ino
@@ -3,18 +3,43 @@
 #define PIN_10 10
 #define PIN_11 11
 
+#define N 15
+
 int DAC_bus[] = {PIN_8, PIN_9, PIN_10, PIN_11};
+
+int v_level = 0;
 
 void test_run();
 
-void test_run2();
+void draw_saw();
+
 
 void setup() {
   DDRB = B1111;      //Set Pins 8-11 as Inputs
+
+    // initialize timer1 
+
+  noInterrupts();        
+
+  TCCR1A = 0;
+
+  TCCR1B = 0;
+
+  TCNT1  = 0;
+
+
+  OCR1A = 125;                //Compare Value
+  TCCR1B |= (1 << WGM12);      // CTC mode
+
+  TCCR1B |= (1<<CS11);    // 8 prescaler 
+
+  TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
+
+  interrupts();         
+
 }
 
-void loop() {
-  test_run2();  
+void loop() { 
 }
 
 
@@ -30,9 +55,11 @@ void test_run(){
   }
 
 //saw tooth test function
-void test_run2(){
-  for (int v_level = 0; v_level < 15; v_level++){
-    PORTB = v_level;         
-    delayMicroseconds(4);
-  }
+void draw_saw(){
+    
+}
+
+ISR(TIMER1_COMPA_vect){
+  PORTB = v_level;         
+  v_level = (v_level + 1) % 16;
 }
