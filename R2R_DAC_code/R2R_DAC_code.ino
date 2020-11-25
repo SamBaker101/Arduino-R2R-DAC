ed2d95b07a7aa02858b0c6f017fb75d08d0175be
Sam Baker <Sam.Baker101@gmail.com>
2020-11-24 23:13:10 -0500

    Added Timer Init
    Moved timer initiation to timer1_init() function

--- R2R_DAC_code/R2R_DAC_code.ino
+++ R2R_DAC_code/R2R_DAC_code.ino
@@ -9,55 +9,33 @@ int DAC_bus[] = {PIN_8, PIN_9, PIN_10, PIN_11};
 
 int v_level = 0;
 
-void test_run();
+void timer1_init();
 
 void draw_saw();
-
 
 void setup() {
   DDRB = B1111;      //Set Pins 8-11 as Inputs
 
-    // initialize timer1 
+  timer1_init();    //Initiate timer
+}
 
+void loop(){
+  }
+
+void timer1_init(){
   noInterrupts();        
-
   TCCR1A = 0;
-
   TCCR1B = 0;
-
   TCNT1  = 0;
-
 
   OCR1A = 125;                //Compare Value
   TCCR1B |= (1 << WGM12);      // CTC mode
-
   TCCR1B |= (1<<CS11);    // 8 prescaler 
-
   TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
 
-  interrupts();         
-
+  interrupts();     
 }
 
-void loop() { 
-}
-
-
-void test_run(){
-  for (int i = 0; i < 4; i++){
-    digitalWrite(DAC_bus[i], HIGH);
-    delay(5);
-    }
-  for (int i = 0; i < 4; i++){
-    digitalWrite(DAC_bus[4-i], LOW);
-    delay(5);
-    }
-  }
-
-//saw tooth test function
-void draw_saw(){
-    
-}
 
 ISR(TIMER1_COMPA_vect){
   PORTB = v_level;         
